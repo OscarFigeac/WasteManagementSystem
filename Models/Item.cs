254bb2ed5c100783d3eddd1457c9f0fb37afbcaf
OscarFigeac <figeacoscar63@gmail.com>
2026-03-24 14:41:38 +0000

    feat(models): Item class added

---
+++ Models/Item.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WasteManagementSystem.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Item name is required")]
+        public string ItemName { get; set; } = string.Empty;
+
+        [Required]
+        public ItemCategory Category { get; set; }
+
+        [Required, DataType(DataType.Date)]
+        [Display(Name = "Date Purchased")]
+        public DateTime PurchaseDate { get; set; }
+
+        [Required, DataType(DataType.Date)]
+        [Display(Name = "Expiry Date")]
+        public DateTime ExpiryDate { get; set; }
+
+        public ItemStatus Status { get; set; } = ItemStatus.InPantry;
+
+        [Required, Range(0.01, 1000.00)] //Monthly estimated value? Could increment if needed for display purposes
+        [Display(Name = "Estimated Value (€)")]
+        public decimal Value { get; set; }
+
+        public int HouseDetailsId { get; set; }
+        public virtual HouseDetails? House { get; set; }
+
+        public virtual WasteLog? WasteLog { get; set; }
+    }
+}
