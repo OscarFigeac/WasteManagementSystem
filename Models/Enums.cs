89c4abe265e26c12d4b6bfa58300deec14fbf3c4
OscarFigeac <figeacoscar63@gmail.com>
2026-03-24 14:16:37 +0000

    Add project files.

---
+++ Models/Enums.cs
@@ -0,0 +1,19 @@
+﻿namespace WasteManagementSystem.Models
+{
+    public enum ItemCategory
+    {
+        Dairy,
+        Vegetable,
+        Meat,
+        Fruit,
+        Pantry,
+        Bakery
+    }
+
+    public enum ItemStatus
+    {
+        InPantry,
+        Consumed,
+        Wasted
+    }
+}
