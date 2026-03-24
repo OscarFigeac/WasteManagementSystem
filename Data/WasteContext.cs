696e6fb2da9621a18b5ff7707e6849e6a6de1fc6
OscarFigeac <figeacoscar63@gmail.com>
2026-03-24 15:00:39 +0000

    feat(DatabaseContext): Context created, tables defined

---
+++ Data/WasteContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WasteManagementSystem.Models;
+
+namespace WasteManagementSystem.Data
+{
+    public class WasteContext : DbContext
+    {
+        public WasteContext(DbContextOptions<WasteContext> options)
+            : base(options)
+        {
+        }
+
+        // These define your database tables
+        public DbSet<HouseDetails> Houses { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<WasteLog> WasteLogs { get; set; }
+    }
+}
+}
