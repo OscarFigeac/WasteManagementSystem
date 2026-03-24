21e23cb3989a5db0e0c2eb89914e982a85c1074a
OscarFigeac <figeacoscar63@gmail.com>
2026-03-24 14:44:33 +0000

    feat(Models): Wastelog entity created

---
+++ Models/WasteLog.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WasteManagementSystem.Models
+{
+    public class WasteLog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ItemId { get; set; }
+
+        [Required(ErrorMessage = "Please provide a reason for the waste")]
+        public string Reason { get; set; } = string.Empty;
+
+        [Required, DataType(DataType.Date)]
+        public DateTime DateWasted { get; set; }
+
+        public virtual Item? Item { get; set; }
+    }
+}
