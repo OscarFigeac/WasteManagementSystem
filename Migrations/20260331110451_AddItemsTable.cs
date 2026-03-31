6f8278699140ed301b8c9edd50201c99bbfd6441
OscarFigeac <figeacoscar63@gmail.com>
2026-03-31 12:15:44 +0100

    feat(db): Data migrations added: -AddItemsTable to update to the new format

---
+++ Migrations/20260331110451_AddItemsTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WasteManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddItemsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
