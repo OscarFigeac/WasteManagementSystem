be46f72e5e67a576287342daf6ac6cb874f624a2
OscarFigeac <figeacoscar63@gmail.com>
2026-03-31 14:05:57 +0100

    feat(DTOs): HouseDTO with fields now created

---
+++ Models/DTOs/HouseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WasteManagementSystem.Models.DTOs
+{
+    public class HouseDTO
+    {
+        public int Id { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public int ItemCount { get; set; }
+    }
+}
