aa3c04d09f14deb932de3b61f20f1fccb148a08f
OscarFigeac <figeacoscar63@gmail.com>
2026-03-31 11:35:51 +0100

    feat(DTOs): Added item DTO to present personalised data to the users insetead of raw database data

---
+++ Models/DTOs/ItemDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WasteManagementSystem.Models.DTOs
+{
+    public class ItemDTO
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string ExpiryStatus { get; set; } = string.Empty;
+        public string DaysRemaining { get; set; } = string.Empty;
+        public decimal FinancialValue { get; set; }
+    }
+}
