4c86c8043ec65d1af9d350abcd44b963635eeae1
OscarFigeac <figeacoscar63@gmail.com>
2026-03-31 12:16:08 +0100

    feat(DTOs): added fields to the dto

--- Models/DTOs/ItemDTO.cs
+++ Models/DTOs/ItemDTO.cs
@@ -2,10 +2,15 @@
 {
     public class ItemDTO
     {
+        public int Id { get; set; }
+
         public string Name { get; set; } = string.Empty;
         public string Category { get; set; } = string.Empty;
         public string ExpiryStatus { get; set; } = string.Empty;
         public string DaysRemaining { get; set; } = string.Empty;
         public decimal FinancialValue { get; set; }
+
+        //This field is exclusively for the purpose of displaying the impact this has on the environment. Update on Controller when necessary.
+        public string WasteImpact { get; set; } = string.Empty;
     }
 }
