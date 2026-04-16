5b0c424b558356167a4c91fd8a9bc1b68a6fac13
OscarFigeac <figeacoscar63@gmail.com>
2026-04-16 20:49:42 +0100

    feat(controllers): AdminControllers implemented

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,14 +1,40 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WasteManagementSystem.Data;
 
 namespace WasteManagementSystem.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    [Authorize(Roles = "Admin")] 
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private readonly WasteContext _context;
+
+        public AdminController(WasteContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var households = await _context.Houses
+                .Include(h => h.Items)
+                .ToListAsync();
+
+            return View(households);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var house = await _context.Houses.FindAsync(id);
+            if (house != null)
+            {
+                _context.Houses.Remove(house);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
