acb4c1e3abb0007240b15bc1ea2ff5359d036483
OscarFigeac <figeacoscar63@gmail.com>
2026-05-04 19:06:41 +0100

    faet(DTO): DTO for mapping an object to the dashboard added

---
+++ Models/DTOs/AccountDashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WasteManagementSystem.Models.DTOs
+{
+    public class AccountDashboardViewModel
+    {
+        public string Eircode { get; set; }
+        public string Address { get; set; }
+        public bool IsPremium { get; set; }
+        public string Role { get; set; }
+    }
+}
